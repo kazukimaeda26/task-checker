4f527d8ffae22b61e9c15230030e96f3532df704
kazukimaeda26 <kazukimaeda26@gmail.com>
2021-05-31 09:52:38 +0900

    headerまでの実装

---
+++ src/components/select/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css"
+
+export const Select = () => {
+  return (
+    <select className="select">
+      <option value={0}>TECH CAMP</option>
+      <option value={1}>sample string</option>
+    </select>
+  );
+};
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Header } from '../../components/header';
+import { Select } from '../../components/select';
 import './style.css';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 
 export const Home = () => {
   return(
-    <div className="main"></div>
+    <div className="main">
+      <Header/>
+      <div className="genre">
+        <Select/>
+        <AddCircleOutlineIcon
+          className="add_circle_outline_icon"
+          fontSize="default"
+        />
+      </div>
+    </div>
   )
 }
