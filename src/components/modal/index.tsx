9ca9b3496b04a4190b969af64702d99f61ebe8c7
kazukimaeda26 <kazukimaeda26@gmail.com>
2021-06-02 08:33:32 +0900

    modalの表示非表示

---
+++ src/components/modal/index.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import Modal from "react-modal";
+
+interface Props {
+  handleClose: () => void;
+  isOpen: boolean;
+}
+
+const customStyles = {
+  overlay: {
+    backgroundColor: "rgb(80, 80, 80, 0.8)",
+  },
+  content: {
+  top: "10%",
+  left: "60%",
+  right: "50%",
+  height: "75vh",
+  width: "20vw",
+  marginLeft: "-30vw",
+  padding: "2vw 10vw",
+  }
+}
+
+export const FormModal = (props: Props) => {
+
+  Modal.setAppElement("#root");
+
+  return (
+    <div>
+      <Modal
+        isOpen={props.isOpen}
+        onRequestClose={props.handleClose}
+        style={customStyles}
+       >
+      </Modal>
+    </div>
+  );
+};
