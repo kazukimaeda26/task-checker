ceefc650b22e84ae9e4d6fb85a2e2ed2588ae382
kazukimaeda26 <kazukimaeda26@gmail.com>
2021-06-03 14:28:17 +0900

    modalのビュー

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -24,7 +24,11 @@ export const Home = () => {
           fontSize="default"
           onClick={handleOpen}
         />
-        <FormModal handleClose={handleClose} isOpen={isOpen} />
+        <FormModal 
+          handleClose={handleClose}
+          isOpen={isOpen}
+          body="genreBody"
+        />
       </div>
       <div className="contents">
         <ToDoList/>
